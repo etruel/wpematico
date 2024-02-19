8e056b8f9e1cce7edadb9074fd021b92eb7392a4
Esteban <etruel@etruel.com>
2024-02-19 11:17:14 -0300

    added filter wpematico_custom_simplepie

--- app/campaign_fetch.php
+++ app/campaign_fetch.php
@@ -159,7 +159,7 @@ class wpematico_campaign_fetch extends wpematico_campaign_fetch_functions {
         $count = 0;
         $prime = true;
 
-        // Access the feed
+        // Access the feed 
         if ($this->campaign['campaign_type'] == "feed" or $this->campaign['campaign_type'] == "youtube" or $this->campaign['campaign_type'] == "bbpress") {
             $wpe_url_feed = apply_filters('wpematico_simplepie_url', $feed, $kf, $this->campaign);
             /**
@@ -176,8 +176,24 @@ class wpematico_campaign_fetch extends wpematico_campaign_fetch_functions {
             $fetch_feed_params = apply_filters('wpematico_fetch_feed_params', $fetch_feed_params, $kf, $this->campaign);
             $simplepie = WPeMatico::fetchFeed($fetch_feed_params);
         } else {
-
+			
+			/**
+			 * DEPRECATED on 2.7 in favor of wpematico_custom_simplepie below. Will be removed on 2.8
+			 */
             $simplepie = apply_filters('Wpematico_process_fetching', $this->campaign);
+			
+			/**
+			 * wpematico_custom_simplepie 
+			 * Filter to make the custom simplepie objects for extra contents that does not have a feed.
+			 * @since 2.7
+			 * @param $simplepie as null or empty because is not used until now. Will be defined in the filter methods.
+			 * @param type object $this = wpematico_campaign_fetch
+			 * @param type string $feed
+			 * @param type number $kf
+			 * @return the created SimplePie Object
+			 */
+            $simplepie = apply_filters('wpematico_custom_simplepie', '', $this, $feed, $kf);
+            
         }
 
         $duplicate_options = WPeMatico::get_duplicate_options($this->cfg, $this->campaign);
