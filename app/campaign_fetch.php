abe626f3dc2727adbcde020751903b1be9d7d8dd
gerarjos14 <gerarmedina1708@gmail.com>
2024-04-25 15:33:06 -0400

    fix wpematico fetch

--- app/campaign_fetch.php
+++ app/campaign_fetch.php
@@ -123,23 +123,23 @@ class wpematico_campaign_fetch extends wpematico_campaign_fetch_functions {
         //hook to add actions and filter on init fetching 
         //add_action('Wpematico_init_fetching', array(__CLASS__, 'my_wpematico_init_fetching') ); 
         add_filter('wpematico_custom_chrset', array('WPeMatico_functions', 'detect_encoding_from_headers'), 999, 1); // move all encoding functions to wpematico_campaign_fetch_functions
-        add_filter('wpematico_after_item_parsers', array($this, 'wpematico_strip_links_a'), 1, 4);
-        add_filter('wpematico_after_item_parsers', array($this, 'wpematico_strip_links'), 2, 4);
-        add_filter('wpematico_after_item_parsers', array($this, 'wpematico_template_parse'), 3, 4);
-        add_filter('wpematico_after_item_parsers', array($this, 'wpematico_campaign_rewrites'), 4, 4);
+        add_filter('wpematico_after_item_parsers', array('wpematico_campaign_fetch_functions', 'wpematico_strip_links_a'), 1, 4);
+        add_filter('wpematico_after_item_parsers', array('wpematico_campaign_fetch_functions', 'wpematico_strip_links'), 2, 4);
+        add_filter('wpematico_after_item_parsers', array('wpematico_campaign_fetch_functions', 'wpematico_template_parse'), 3, 4);
+        add_filter('wpematico_after_item_parsers', array('wpematico_campaign_fetch_functions', 'wpematico_campaign_rewrites'), 4, 4);
 
         if ($this->campaign['campaign_type'] == "youtube") {
-            add_filter('wpematico_get_post_content_feed', array($this, 'wpematico_get_yt_rss_tags'), 999, 4);
-            add_filter('wpematico_get_item_images', array($this, 'wpematico_get_yt_image'), 999, 4);
-            add_filter('wpematico_excludes', array($this, 'wpematico_exclude_shorts'), 10, 4);
+            add_filter('wpematico_get_post_content_feed', array('wpematico_campaign_fetch_functions', 'wpematico_get_yt_rss_tags'), 999, 4);
+            add_filter('wpematico_get_item_images', array('wpematico_campaign_fetch_functions', 'wpematico_get_yt_image'), 999, 4);
+            add_filter('wpematico_excludes', array('wpematico_campaign_fetch_functions', 'wpematico_exclude_shorts'), 10, 4);
         }
         if ($this->cfg['add_extra_duplicate_filter_meta_source'] && !$this->cfg['disableccf']) {
             add_filter('wpematico_duplicates', array($this, 'WPeisDuplicatedMetaSource'), 10, 3);
         }
         if (isset($this->images_options['fifu']) && $this->images_options['fifu']) {
-            add_filter('wpematico_set_featured_img', array($this, 'url_meta_set_featured_image'), 999, 2);
-            add_filter('wpematico_get_featured_img', array($this, 'url_meta_set_featured_image'), 999, 2);
-            add_filter('wpematico_item_filters_pos_img', array($this, 'url_meta_set_featured_image_setmeta'), 999, 2);
+            add_filter('wpematico_set_featured_img', array('wpematico_campaign_fetch_functions', 'url_meta_set_featured_image'), 999, 2);
+            add_filter('wpematico_get_featured_img', array('wpematico_campaign_fetch_functions', 'url_meta_set_featured_image'), 999, 2);
+            add_filter('wpematico_item_filters_pos_img', array('wpematico_campaign_fetch_functions', 'url_meta_set_featured_image_setmeta'), 999, 2);
         }
     }
         /**
