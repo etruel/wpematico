f3648c4ab7eee30bb15c7b59a5da77b686071f69
gerarjos14 <gerarmedina1708@gmail.com>
2024-05-14 19:12:21 -0400

    correction to messages

--- app/wpematico_functions.php
+++ app/wpematico_functions.php
@@ -621,9 +621,9 @@ if (!class_exists('WPeMatico_functions')) {
 				$proplugin_data = self::plugin_get_version($plpath);
 
 				if ($proplugin_data['Name'] == 'WPeMatico Professional' && version_compare($proplugin_data['Version'], WPeMatico::PROREQUIRED, '<')) {
-					$message .= __('You are using WPeMatico Professional too old.', 'wpematico') . '<br />';
+					$message .= __('Your current version of WPeMatico Professional does not support WPeMatico ', 'wpematico') . WPeMaticoPRO::COREREQUIRED . '<br />';
 					$message .= __('Must install at least "WPeMatico Professional" ' . WPeMatico::PROREQUIRED, 'wpematico');
-					$message .= ' <a href="' . admin_url('plugins.php?page=wpemaddons') . '#wpematico-pro"> ' . __('Go to upgrade Now', 'wpematico') . '</a>';
+					$message .= ' <a href="' . admin_url('plugins.php?page=wpemaddons') . '#wpematico-pro"> ' . __('Go to update Now', 'wpematico') . '</a>';
 					$message .= '<script type="text/javascript">jQuery(document).ready(function($){$("#wpematico-pro").css("backgroundColor","yellow");});</script>';
 					//Commented to allow access to the settings page
 					//$checks=false;
