7133a6193364b6f5ca7e9d4d2c5107b94e3ec341
gerarjos14 <gerarmedina1708@gmail.com>
2024-05-17 21:14:33 -0400

    fix show metaboxes

--- app/campaign_edit_functions.php
+++ app/campaign_edit_functions.php
@@ -199,10 +199,10 @@ class WPeMatico_Campaign_edit_functions {
 	}
 	static function campaign_type_options() {
 		$options=array(
-			array( 'value'=> 'feed', 'text' => __('Feed Fetcher (Default)', 'wpematico' ), "show"=>array('feeds-box') ),
+			array( 'value'=> 'feed', 'text' => __('Feed Fetcher (Default)', 'wpematico' ), "show"=>array('feeds-box', 'audios-box','videos-box','cron-box','template-box', 'images-box') ),
 			array( 'value'=> 'youtube','text' => __('You Tube Fetcher', 'wpematico' ), "show"=>array('feeds-box','youtube-box') ),
-			array( 'value'=> 'bbpress','text' => __('bbPress Forums', 'wpematico' ), "show"=>array('feeds-box','bbpress-box') ),
-			array( 'value'=> 'xml','text' => __('XML Campaign Type', 'wpematico' ), "show"=>array('xml-campaign-box') ),
+			array( 'value'=> 'bbpress','text' => __('bbPress Forums', 'wpematico' ), "show"=>array('feeds-box','bbpress-box', 'audios-box','videos-box','cron-box','template-box', 'images-box') ),
+			array( 'value'=> 'xml','text' => __('XML Campaign Type', 'wpematico' ), "show"=>array('xml-campaign-box', 'audios-box','videos-box','cron-box','template-box', 'images-box') ),
 			);
 		$options = apply_filters('wpematico_campaign_type_options', $options);
 
