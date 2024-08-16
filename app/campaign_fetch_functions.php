abb90b72764dfd2cb2e656b9ddee088ee30ec31f
gerarjos14 <gerarmedina1708@gmail.com>
2024-08-16 17:26:19 -0400

    fix skip items

--- app/campaign_fetch_functions.php
+++ app/campaign_fetch_functions.php
@@ -69,7 +69,7 @@ class wpematico_campaign_fetch_functions {
 		$post_id		 = $this->campaign_id;
 		$current_item	 = apply_filters('wpematico_item_parsers', $current_item, $campaign, $feed, $item);
 		if (isset($current_item['SKIP']) && is_int($current_item['SKIP']))
-			return $current_item['SKIP'];
+			return $current_item;
 			
 		// strip all HTML tags before apply filter wpematico_after_item_parsers
 		if ($campaign['campaign_striphtml']) {
