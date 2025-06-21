5504fba5a9b79e874e8e014b0f428e8cdbde0f66
Esteban <etruel@etruel.com>
2025-06-21 18:45:25 -0300

    Descartar cambios en wpematico_class.php para merge con master

--- wpematico_class.php
+++ wpematico_class.php
@@ -461,6 +461,7 @@ if (!class_exists('WPeMatico')) {
 				$cfg['disable_extensions_feed_page'] = true;
 			}else{
 				$cfg['disable_extensions_feed_page'] = (!isset($options['disable_extensions_feed_page']) || empty($options['disable_extensions_feed_page'])) ? false : ( ($options['disable_extensions_feed_page'] == 1) ? true : false );
+
 			}
 				
 			//Disable Extensions feed Page. 
